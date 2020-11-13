e0772a77fb4ffbcf56a43b131386f4ccd0bed480
snigdhagupta <snigi.gupta@gmail.com>
2020-11-12 21:36:30 -0500

    Work from 11/9-11/12 finished caching and image compression

--- app.py
+++ app.py
@@ -1,74 +1,89 @@
 from flask import Flask, render_template, request, abort, send_file
 from werkzeug.utils import secure_filename
+from cachetools import cached, TTLCache
+from PIL import Image, ImageDraw
 import os
 import io
 import uuid
 import cv2
+import numpy as np
 
 # file paths
-absolute_path = os.path.dirname(__file__) #D:\UB CSE\ImageProcessor\
-template_dir = os.path.join(absolute_path, 'assets') #D:\UB CSE\ImageProcessor\assets
+absolute_path = os.path.dirname(__file__)  # D:\UB CSE\ImageProcessor\
+template_dir = os.path.join(absolute_path, 'assets')  # D:\UB CSE\ImageProcessor\assets
 
 # app configurations
 app = Flask(__name__, template_folder=template_dir)
+cache = TTLCache(maxsize=1000, ttl=60)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['jpg', 'png', 'gif', 'jpeg']
 IMAGE_FOLDER = os.path.join('static', 'images')
-app.config['IMAGE_DIR'] = IMAGE_FOLDER #static\images
+app.config['IMAGE_DIR'] = IMAGE_FOLDER  # static\images
+
 
 # route to render upload.html template
 @app.route('/')
 def upload():
     return render_template("upload.html")
 
+
 # route to display submitted image
 @app.route('/image', methods=['GET', 'POST'])
 def upload_file():
-
     # saving file with unique uuid
     if request.method == 'POST':
         f = request.files['file']
         temp_file_name = f.filename
+        f = Image.open(f).convert("RGB")
         if temp_file_name != '':
             unique_filename = str(uuid.uuid4())
             file_extension = temp_file_name.split('.')[1]
-            # file_extension = f.content_type
             if file_extension not in app.config['UPLOAD_EXTENSIONS']:
                 abort(400)
-            file_name = '.'.join([secure_filename(unique_filename), file_extension]) #5d8fe035-d9b0-4066-9334-53ec85f798df.jpeg
+            file_extension = 'webp'
+            file_name = '.'.join(
+                [secure_filename(unique_filename), file_extension])  # 5d8fe035-d9b0-4066-9334-53ec85f798df.jpeg
             file_path = os.path.join(app.config['IMAGE_DIR'], file_name)
-            f.save(os.path.join(absolute_path, file_path))
-            # convert to webp format
-            # import pdb
-            # pdb.set_trace();
-            # _img = cv2.imread(file_path)
-            # print(_img)
-            # retval, buf = cv2.imencode(".webp", _img,[cv2.IMWRITE_WEBP_QUALITY, 100])
-            # print(retval)
-            # print("webp--->", os.path.join(app.config['IMAGE_DIR'], '.'.join([secure_filename(unique_filename), 'webp'])))
-            # buf.tofile()
-            # flag = cv2.imwrite('test.webp', buf.tofile('Extens'))
-            # print(flag)
-            # return 'file uploaded successfully'
+            f.save(os.path.join(absolute_path, file_path), 'webp')
             url = "http://127.0.0.1:5000/edit/" + file_name
-            return render_template("imageurl.html", images_dir=file_path, image_url=url, file_name=file_name)
-            # return send_file(os.path.join(images_dir, file_name), mimetype='image/jpeg')
+            return render_template("edit.html", images_dir=file_path, image_url=url, file_name=file_name)
 
-# route to display submitted image
+
+# caching function to display edited image
+@cached(cache)
+def edit_file(file_name, img_height, img_width, img_rotate, img_ellipse=False):
+    file_path = os.path.join(app.config['IMAGE_DIR'], file_name)
+    file_extension = file_name.split('.')[1]
+    url = "http://127.0.0.1:5000/edit/" + file_name
+
+    # get original image
+    org_img = Image.open(file_path).convert("RGB")
+
+    # get original image dimensions if no URL parameters
+    org_height, org_width = org_img.size
+
+    # resizing the image
+    dsize = (int(img_width or org_width), int(img_height or org_height))
+    new_img = org_img.resize(dsize)
+
+    
+
+    new_filename = '.'.join([secure_filename(str(uuid.uuid4())), file_extension])
+    new_file_path = os.path.join(app.config['IMAGE_DIR'], new_filename)
+    new_img.save(new_file_path, 'webp')
+    return new_file_path, url, file_name
+
+
+# route to display edited image
 @app.route('/edit/<file_name>', methods=['GET', 'POST'])
 def edit(file_name):
     img_height = request.args.get('height')
     img_width = request.args.get('width')
-    file_path = os.path.join(app.config['IMAGE_DIR'], file_name)
-    file_extension = file_name.split('.')[1]
-    url = "http://127.0.0.1:5000/edit/" + file_name
-    org_img = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)
-    dsize = (int(img_width),int(img_height))
-    new_img = cv2.resize(org_img,dsize)
-    new_filename = '.'.join([secure_filename(str(uuid.uuid4())), file_extension])
-    new_file_path = os.path.join(app.config['IMAGE_DIR'], new_filename)
-    cv2.imwrite(new_file_path, new_img)
-    return render_template("imageurl.html", images_dir=new_file_path, image_url=url, file_name=file_name)
+    img_ellipse = request.args.get('ellipse')
+    img_rotate = request.args.get('rotate')
+
+    new_file_path, url, new_file_name = edit_file(file_name, img_height, img_width, img_rotate, img_ellipse)
+    return render_template("edit.html", images_dir=new_file_path, image_url=url, file_name=new_file_name)
 
 
 if __name__ == "__main__":
